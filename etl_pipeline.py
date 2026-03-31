982d27bd4a5b09a87afba85b6b72a64c7d41c9fc
Komalavalli-G <komalakirthi1999@gmail.com>
2026-03-31 18:42:59 +0530

    Update etl_pipeline.py

--- etl_pipeline.py
+++ etl_pipeline.py
@@ -1,25 +1,35 @@
 import pandas as pd
+import logging
 
-# Extract
-df = pd.read_csv('../data/patients.csv')
+# Logging setup
+logging.basicConfig(
+    filename='etl.log',
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
 
-print("Original Data:")
-print(df)
+try:
+    logging.info("ETL Job Started")
 
-# Transform (Data Cleaning)
-df = df.drop_duplicates()
+    # Extract
+    df = pd.read_csv('../data/patients.csv')
+    logging.info("Data extracted successfully")
 
-df['name'] = df['name'].fillna('Unknown')
-df['age'] = df['age'].fillna(df['age'].mean())
-df['city'] = df['city'].fillna('Unknown')
+    # Transform
+    df = df.drop_duplicates()
+    df['name'] = df['name'].fillna('Unknown')
+    df['age'] = df['age'].fillna(df['age'].mean())
+    df['city'] = df['city'].fillna('Unknown')
 
-# Data Quality Check
-if df.isnull().sum().sum() == 0:
-    print("No missing values ✅")
-else:
-    print("Data still has missing values ❌")
+    # Data Quality Check
+    if df.isnull().sum().sum() == 0:
+        logging.info("No missing values found")
+    else:
+        logging.warning("Missing values still present")
 
-# Load
-df.to_csv('../output/cleaned_patients.csv', index=False)
+    # Load
+    df.to_csv('../output/cleaned_patients.csv', index=False)
+    logging.info("Data loaded successfully")
 
-print("Cleaned data saved successfully!")
+except Exception as e:
+    logging.error(f"ETL Job Failed: {e}")
